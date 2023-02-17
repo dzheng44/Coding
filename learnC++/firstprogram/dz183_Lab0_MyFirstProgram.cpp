74ae2cd374b9770a258f3a13e1703dacfc0f35c5
dzheng44 <33125744+dzheng44@users.noreply.github.com>
2023-02-17 12:22:15 -0600

    Add files via upload

---
+++ learnC++/firstprogram/dz183_Lab0_MyFirstProgram.cpp
@@ -0,0 +1,18 @@
+/*
+Program Title: A Little Bit about Myself
+Author: David Zheng
+Description: This program outputs a short paragraph about myself.
+*/
+
+#include <iostream>
+
+using std::cout;
+
+int main()
+{
+	cout << "Hello World! My name is David Zheng!\n";
+	cout << "I am from Meridian, Mississippi. ";
+	cout << "My favorite past time is playing video games, hanging out ";
+	cout << "with friends and reading novels. ";
+	cout << "My favorite color is violet/blue.\n";
+}
